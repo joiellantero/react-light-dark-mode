4fc90bd1f18d00bcb83341a9fc02790790339b27
jllantero <jllantero@gmail.com>
2021-01-20 18:12:25 +0800

    added material ui

--- src/Header/Header.component.js
+++ src/Header/Header.component.js
@@ -1,8 +1,30 @@
 import React from 'react';
+
 import { useRecoilState } from 'recoil';
 import { darkModeState } from '../Shared/globalState';
 
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+}));
+
 const Header = () => {
+    const classes = useStyles();
     const [isDarkModeEnabled, setISDarkModeEnabled] = useRecoilState(darkModeState);
     const toggleDarkMode = () => setISDarkModeEnabled(!isDarkModeEnabled);
 
@@ -25,6 +47,22 @@ const Header = () => {
                     />
                 </div>
             </nav>
+
+            <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        React Webapp
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+            </div>
+
+            
         </>
     );
 };
